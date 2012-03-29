2af942a359ea6f0a0114cad3884e2258eaf82264
fandro <weibo.fu@gmail.com>
2012-03-29 18:19:56 +0800

    添加web功能

---
+++ src/main/java/controller/copy/HelloWordController.java
@@ -0,0 +1,15 @@
+package controller.copy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWordController {
+	
+	@RequestMapping("/hello")
+	public ModelAndView helloWord(){
+		String msg = "hello!my first spring mvc!";
+		return new ModelAndView("hello", "message", msg);
+	}
+}
