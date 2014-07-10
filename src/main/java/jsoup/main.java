a8c92e60142cc62eb008c7f570280b563befbaad
fuweibo <fwbion1004@163.com>
2014-07-10 18:02:09 +0800

    jc

--- src/main/java/jsoup/main.java
+++ src/main/java/jsoup/main.java
@@ -22,11 +22,58 @@ public class main {
 		//"ul>class:ztCollectImg>li>img";
 		
 		Elements e = doc.getElementsByClass("ztCollectImg");
-//		e = doc.getElementsByClass("track");
 		for (Iterator iterator = e.iterator(); iterator.hasNext();) {
 			Element element = (Element) iterator.next();
-			String imgHtml = element.child(0).child(0).outerHtml();
-			System.out.println(imgHtml);
+			Element ec = element.child(0).child(0);
+			Element ec2 = element.child(0);
+			
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.getElementsByTag(\"a\").attr(\"href\")="+ec.getElementsByTag("a").attr("href"));
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.getElementsByTag(\"img\").attr(\"original\")="+ec.getElementsByTag("img").attr("original"));
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.ec.nextElementSibling().text()="+ec.nextElementSibling().text());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.ec.nextElementSibling().ownText()="+ec.nextElementSibling().ownText());
+			
+			
+			System.out.println("ec2.nextSibling()="+ec2.nextSibling());
+			System.out.println("ec.nextElementSibling()="+ec.nextElementSibling());
+			
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.data()="+ec.data());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.html()="+ec.html());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.nodeName()="+ec.nodeName());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.outerHtml()="+ec.outerHtml());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.ownText()="+ec.ownText());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.siblingIndex()="+ec.siblingIndex());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.tagName()="+ec.tagName());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.text()="+ec.text());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.val()="+ec.val());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.elementSiblingIndex()="+ec.elementSiblingIndex());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.childNode(0)="+ec.childNode(0));
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.getElementsByTag(\"a\")="+ec.getElementsByTag("a"));
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.getElementsByTag(\"img\").outerHtml()="+ec.getElementsByTag("img").outerHtml());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.select(\"a[href]\").toString()"+ec.select("a[href]").toString());
+			System.out.println("********************************************************************************************");
+			System.out.println("ec.select(\"img[original]\").toString()"+ec.select("img[original]").toString());
+//			System.out.println(""+ec.);
 		}
+		//获取连接地址，img地址，文本描述
+		
+		
 	}
 }
