7a6a62cc258c256a8a2ceee64f63a6806bd34800
fwb <fwbion1004@163.com>
2018-11-02 14:56:53 +0800

    修改 stream

--- src/main/com/javacore/lambda/LambdaTest.java
+++ src/main/com/javacore/lambda/LambdaTest.java
@@ -1,10 +1,48 @@
 package javacore.lambda;
 
+import org.junit.Test;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
- * TODO
+ *
+ * 1. 函数接口，接口中只有一个方法
+ * 2. 参数类型推断机制
  *
  * @author fuwb
  * @date 2018/4/27
  */
 public class LambdaTest {
+
+    /**
+     * lambda表达式书写
+     */
+    @Test
+    public void baseInfoTest() {
+
+        // 1.1 原来写法
+        Runnable runnable1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("enhen!");
+            }
+        };
+
+        // 1.2 lambda新写法,无参函数简写
+        Runnable runnable2 = () -> System.out.println("enhen!");
+
+        // 2.1 原来写法
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+
+        // 2.2 lambda写法
+        ActionListener listener2 = event -> System.out.println("button clicked");
+
+
+    }
 }
--- src/main/com/javacore/stream/OptionalTest.java
+++ src/main/com/javacore/stream/OptionalTest.java
@@ -1,7 +1,18 @@
 package javacore.stream;
 
 /**
- * TODO
+ * Optional
+ * <pre>
+ *  常用方法：
+ *   isPresent() : 判断是否为空，不建议使用
+ *         get() ：获取对象，如果是null则报异常
+ *  public<U> Optional<U> map(Function<? super T, ? extends U> mapper) :
+ *  orElse(T other) :
+ *  orElseGet(Supplier<? extends T> other) : 否则获取对象
+ *  filter : 过滤
+ *  flatMap : 集合二维转一维
+ *  orElseThrow : 否则抛出异常
+ * </pre>
  *
  * @author fuwb
  * @date 2018/4/27
